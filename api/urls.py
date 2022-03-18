dbf6b0cf1e6baa834b9cde45dfaba13610e46c32
Anthony Jones <70544335+ajones00@users.noreply.github.com>
2022-03-17 23:16:27 -0400

    fix

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,5 @@ from .views import HomeView, CreateHomeView, GetHome
 urlpatterns = [
     path('home', HomeView.as_view()),
     path('create-home', CreateHomeView.as_view()),
-    path('get-home', GetHome.as_view)
+    path('get-home', GetHome.as_view())
 ]
